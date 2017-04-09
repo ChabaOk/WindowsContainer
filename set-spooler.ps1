7f9a0c7b357db2bac90f30f5e0fa332553278dee
ChabaOk <chaba_ok@yahoo.com>
2017-04-09 15:48:03 +0700

    Add files via upload

---
+++ set-spooler.ps1
@@ -0,0 +1,19 @@
+﻿Configuration ManagePrintServer
+{
+    param (
+        [string[]]$NodeName = 'localhost'
+    )
+
+    Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+    Node $NodeName
+    {
+        Service StartSpooler
+        {
+            Name = "spooler"
+            State = "running"
+            Ensure = "Present"
+        }
+    }
+}
+ManagePrintServer -NodeName localhost -OutputPath C:\mof
+Start-DscConfiguration -Path C:\mof -Verbose -wait
